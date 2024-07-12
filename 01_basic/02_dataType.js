835810d37d66667b9b081c4482b11778cd7e93a9
Manmohan Singh <117078139+Manmohansinghyadav@users.noreply.github.com>
2024-07-12 04:19:14 +0000

    all data type

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code in new version
+
+// alert(3+3) // we are using node.js not brouser that by alert is not working
+
+console.log(3 
+    +3)  //code readibility that should be higher
+
+console.log("Manmohan singh")
+
+let  name = "mohan"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigInt
+// string in => ""
+//boolean => false / true
+//null => stand alone value 
+//undefine => 
+// symbol => unique ness
+
+//object
+
+console.log(typeof "mohan")
+console.log(typeof age)
+console.log(typeof null);
+console.log(typeof undefined);
+
