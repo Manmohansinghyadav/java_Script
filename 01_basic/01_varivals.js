92a6981fe836f5a34c6a1b131ea93962a57b820c
Manmohan Singh <117078139+Manmohansinghyadav@users.noreply.github.com>
2024-07-12 03:57:27 +0000

    learn about variables

---
+++ 01_basic/01_varivals.js
@@ -0,0 +1,16 @@
+const accountId = 14433
+let accountEmail = "maohan@gmail.com"
+var accountPass = "0983736"
+ accountCity = "Ghaziabad"
+let accountState =""
+
+//  accountId = 20 // not allowed
+accountEmail = "mha@google.com"
+accountPass = "121212"
+accountCity = "Delhi"
+
+/*
+note:  not use var
+*/
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPass.accountCity,accountState]);
