c6057b3beb4950016acec042e17b4d339f583d36
Manmohan Singh <117078139+Manmohansinghyadav@users.noreply.github.com>
2024-07-12 05:14:28 +0000

    Opraters more

--- 01_basic/03_ConversionOpratour.js
+++ 01_basic/03_ConversionOpratour.js
@@ -24,5 +24,59 @@ let boolenIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//****************************** Operations **************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4); // ** { Means 2 the power 2 }
+// console.log(2/2);
+// console.log(2%2); // remainder
+
+let str1 = "hello"
+let str2 = " mohan"
+let str3 = str1 +  str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 * 5) + 8 % 9);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 ,num2, num3
+num1=num2=num3 = 2+2
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+
+let gameCounter = 100
+gameCounter++
+// console.log(gameCounter);
+
+var str = "Hello World";
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+var str = "Hello World";
+// console.log(str.replace("World", "Universe"));
+
+var str = "Hello World";
+// console.log(str.indexOf("W"));
+
+var str = "Hello World";
+console.log(str.slice(6));
+console.log(str.slice(2));
+console.log(str.slice(8));
+
