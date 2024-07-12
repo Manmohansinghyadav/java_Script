a48c2ba5fa825f9e8089d44d7478f0de688b5407
Manmohan Singh <117078139+Manmohansinghyadav@users.noreply.github.com>
2024-07-12 04:40:35 +0000

    conversions in js

---
+++ 01_basic/03_ConversionOpratour.js
@@ -0,0 +1,28 @@
+let score = "33asd"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number (score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+//"33abd" => NaN
+//true => 1 and false => 0
+
+
+let isLoggedIn = 1 
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(boolenIsLoggedIn)
+
+// 1 => true; 0=> false
+//"" => false
+// "mohan" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
